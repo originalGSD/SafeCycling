b9a8cae97b8d160685f54937f70e4d75b6a826ac
psychedelictripzxc <>
2018-11-23 06:09:51 +0800

    Implement movements on cars

---
+++ Assets/Movement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public float moveSpeed = 0.1f;
+    public Vector3 moveVector;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(moveVector * moveSpeed);
+    }
+}
