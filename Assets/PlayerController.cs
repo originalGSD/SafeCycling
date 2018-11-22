5f7f55add86c005da56525a52f736d11f29f1e3c
Muhammmad Syazwan Bin Suhami Bin Suhami <1700585@sit.singaporetech.edu.sg>
2018-11-22 21:33:36 +0800

    Added Camera inside
    Added camera in and more tree assets

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton("Fire1"))
+        {
+            Vector3 newCameraPosition = transform.position + GameObject.FindGameObjectWithTag("MainCamera").transform.forward * 10 * Time.deltaTime;
+           transform.position = new Vector3(newCameraPosition.x, transform.position.y, newCameraPosition.z);
+        }
+	}
+}
