541e878d6435e7041e691156ba494996d53024bc
Saiwoon <1701567@sit.singaporetech.edu.sg>
2018-11-24 18:41:19 +0800

    Bug fixes and walk fix

--- Assets/VRLookWalk.cs
+++ Assets/VRLookWalk.cs
@@ -7,9 +7,9 @@ public class VRLookWalk : MonoBehaviour
 
     public Transform vrCamera;
 
-    public float toggleAngle = 30.0f;
+    public float toggleAngle = 10.0f;
 
-    public float speed = 3.0f;
+    public float speed = 4.0f;
 
     public bool moveForward;
 
@@ -25,19 +25,9 @@ public class VRLookWalk : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (vrCamera.eulerAngles.x >= toggleAngle && vrCamera.eulerAngles.x < 90.0f)
-        {
-            moveForward = true;
-        }
-        else
-        {
-            moveForward = false;
-        }
 
-        if (moveForward)
-        {
             Vector3 forward = vrCamera.TransformDirection(Vector3.forward);
             cc.SimpleMove(forward * speed);
-        }
+
     }
 }
